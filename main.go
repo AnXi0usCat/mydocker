a0db6437832053f2f6dd08fe891e70709c739fdd
Mihhail Fjodorov <mikhail.fjodorov@gmail.com>
2023-12-09 17:48:34 +0000

    lint

--- main.go
+++ main.go
@@ -12,19 +12,19 @@ func main() {
 	args := os.Args[4:len(os.Args)]
 
 	cmd := exec.Command(command, args...)
-    
-    cmd.Stdin = os.Stdin
+
+	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 
 	if err := cmd.Run(); err != nil {
-        log.Printf("Program failed %s", err.Error())
-        switch err := err.(type) {
-        case *exec.ExitError:
-            os.Exit(err.ExitCode())
-        default:
-		    log.Printf("Child process exited abnormally: %v", err)
-            os.Exit(255)
-        }
+		log.Printf("Program failed %s", err.Error())
+		switch err := err.(type) {
+		case *exec.ExitError:
+			os.Exit(err.ExitCode())
+		default:
+			log.Printf("Child process exited abnormally: %v", err)
+			os.Exit(255)
+		}
 	}
 }
