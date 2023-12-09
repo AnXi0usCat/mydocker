138fd9c80f034c51fc5290b5de137ba36352cd5d
Mihhail Fjodorov <mikhail.fjodorov@gmail.com>
2023-12-09 17:48:01 +0000

    tie command exist code to the parent process exit code

--- main.go
+++ main.go
@@ -12,11 +12,19 @@ func main() {
 	args := os.Args[4:len(os.Args)]
 
 	cmd := exec.Command(command, args...)
-
+    
+    cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 
 	if err := cmd.Run(); err != nil {
-		log.Fatalf("Go error when running command: %v", err)
+        log.Printf("Program failed %s", err.Error())
+        switch err := err.(type) {
+        case *exec.ExitError:
+            os.Exit(err.ExitCode())
+        default:
+		    log.Printf("Child process exited abnormally: %v", err)
+            os.Exit(255)
+        }
 	}
 }
