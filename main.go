b7c30ef6732d8623b0f1e80a12b00368622fdb5c
Mihhail Fjodorov <mikhail.fjodorov@gmail.com>
2023-12-10 20:02:32 +0000

    function to copy the comma d file to chroot jail

--- main.go
+++ main.go
@@ -1,18 +1,55 @@
 package main
 
 import (
+	"io"
 	"log"
 	"os"
 	"os/exec"
 )
 
-// Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
+func copyTo(to, from string) error {
+    ffi, err := os.Stat(from)
+    if err != nil {
+        log.Printf("failed to get the file stats %v", err)
+        return err
+    }
+    
+    fromFile, err := os.Open(from)
+    if err != nil {
+        log.Printf("failed to open the origanal command file %v", err)
+        return err
+    }
+    defer fromFile.Close()
+
+    toFile, err := os.Create(to)
+    if err != nil {
+        log.Printf("failed to create a destination file for the command %v", err)
+        return err
+    }
+    defer toFile.Close()
+
+    _, err = io.Copy(toFile, fromFile)
+    if err != nil {
+        log.Printf("failed to copy contents of the command file %v", err)
+        return err
+    }
+
+    err = os.Chmod(to, ffi.Mode())
+    if err != nil {
+        log.Printf("failed to grant permissions to  target command file %v", err)
+        return err
+    }
+
+    return nil
+}
+
+// Usage: ./mydocker run <image> <command> <arg1> <arg2> ...
 func main() {
 	command := os.Args[3]
 	args := os.Args[4:len(os.Args)]
 
 	cmd := exec.Command(command, args...)
-
+    
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
