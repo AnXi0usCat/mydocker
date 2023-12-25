8006acfc931d4901f678b7585902ae2c2837a712
Mihhail Fjodorov <mikhail.fjodorov@gmail.com>
2023-12-25 20:04:09 +0000

    reaftor the chroot

--- main.go
+++ main.go
@@ -1,88 +1,76 @@
 package main
 
 import (
-	"io"
 	"log"
 	"os"
 	"os/exec"
-	"path/filepath"
-	"syscall"
 )
 
-func copyTo(to, from string) error {
-	ffi, err := os.Stat(from)
-	if err != nil {
-		log.Printf("failed to get the file stats %v", err)
-		return err
-	}
+func createRootDir() (string, error) {
+    jail := "jail"
+    command :=  exec.Command("mkdir", "-p", jail)
+    err := command.Run()
+    if err != nil {
+        log.Printf("Failed to create a temp directory %v", err)
+        return "",  err
+    }
+    return jail, nil
+}
 
-	fromFile, err := os.Open(from)
-	if err != nil {
-		log.Printf("failed to open the origanal command file %v", err)
-		return err
-	}
-	defer fromFile.Close()
+func copyDir(command string, jail string) {
+    mkDirC := exec.Command("mkdir", "-p",  jail + "/usr/local/bin")
+    mkDirC.Stdout = os.Stdout
+    mkDirC.Stderr = os.Stderr
+    
+    err := mkDirC.Run()
+    if err !=nil {
+        log.Printf("Failed on mkdr -p %v", jail)
+        panic("Failed on mkdr -p " + jail)
+    }
+    
+    cp := exec.Command("cp", command, jail + "/usr/local/bin")
+    cp.Stdout = os.Stdout
+    cp.Stderr = os.Stderr
+    cp.Stdin = os.Stdin
+    
+    err = cp.Run()
+    if err !=nil {
+        log.Printf("Failed on cp %v /usr/local/bin", command)
+        panic("Failed on cp %v /usr/local/bin" + jail)
+    }
+}
 
-	toFile, err := os.Create(to)
-	if err != nil {
-		log.Printf("failed to create a destination file for the command %v", err)
-		return err
-	}
-	defer toFile.Close()
+func runCommand(jail string, command string, args []string) error {
+    chRootArgs := []string {jail, command}
+    chRootArgs = append(chRootArgs, args...)
 
-	_, err = io.Copy(toFile, fromFile)
-	if err != nil {
-		log.Printf("failed to copy contents of the command file %v", err)
-		return err
-	}
+    cmd := exec.Command("chroot", chRootArgs...)
+    cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
 
-	err = os.Chmod(to, ffi.Mode())
-	if err != nil {
-		log.Printf("failed to grant permissions to  target command file %v", err)
-		return err
-	}
-
-	return nil
+    err := cmd.Run()
+    if err != nil {
+        log.Printf("failed to run chroot cmd: %v, args %v", command, args)
+        return err
+    }
+    return nil
 }
 
 // Usage: ./mydocker run <image> <command> <arg1> <arg2> ...
 func main() {
+    log.Printf("statrting the command")
 	command := os.Args[3]
 	args := os.Args[4:len(os.Args)]
 
-	// create a chroot jail
-	tmp, err := os.MkdirTemp(os.TempDir(), "*")
+    jail, err := createRootDir()
+    if err != nil {
+        panic("could not create a target directory, stopping execution")
+    }
+    copyDir(command, jail)
+    err = runCommand(jail, command, args)
+
 	if err != nil {
-		log.Fatalf("Failed to create a new root directory %v", err)
-	}
-	defer os.RemoveAll(tmp)
-
-	// find executable for the command we are tring to Run
-	command, err = exec.LookPath(command)
-	if err != nil {
-		log.Fatalf("Failed to find executable of the command %v", err)
-	}
-
-	// copy the command executable to chroot jail
-	commandChRoot := filepath.Join(tmp, filepath.Base(command))
-	err = copyTo(commandChRoot, command)
-	if err != nil {
-		log.Fatalf("Failed to copy the command %v", err)
-	}
-
-	err = syscall.Chroot(tmp)
-	if err != nil {
-		log.Fatalf("Failed to chroot in the temp directory %v", err)
-	}
-
-	commandChRoot = filepath.Join("/", filepath.Base(command))
-	cmd := exec.Command(commandChRoot, args...)
-
-	cmd.Stdin = os.Stdin
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-
-	if err := cmd.Run(); err != nil {
 		log.Printf("Program failed %s", err.Error())
 		os.Exit(cmd.ProcessState.ExitCode())
 	}
